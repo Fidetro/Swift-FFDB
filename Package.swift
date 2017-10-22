cac9524b16938e08bf745e23840a7e75693fa910
fidetro <zykzzzz@hotmail.com>
2017-10-22 11:58:51 +0800

    eidt Package

--- Package.swift
+++ Package.swift
@@ -4,4 +4,6 @@ let package = Package(
 	name: "SwiftFFDB",
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3)
-	])
+	],
+	exclude:["Podfile","Podfile.lock","Pods","src","Swift-FFDB","Swift-FFDB.xcodeproj","Swift-FFDB.xcworkspace","Swift-FFDBTests","SwiftFFDB.podspec"]
+)
