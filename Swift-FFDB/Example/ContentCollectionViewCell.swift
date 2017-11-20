5095d34328be38c46296fd782d9028ed2a0f560b
fidetro <zykzzzz@hotmail.com>
2017-11-20 12:03:26 +0800

    add example

---
+++ Swift-FFDB/Example/ContentCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ContentCollectionViewCell.swift
+//  devSwiftFFDB_Example
+//
+//  Created by Fidetro on 2017/10/19.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ContentCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var contentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Swift-FFDB/Example/TableModel.swift
@@ -0,0 +1,51 @@
+//
+//  TableModel.swift
+//  devSwiftFFDB_Example
+//
+//  Created by Fidetro on 2017/10/18.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+import UIKit
+
+
+
+struct FFShop :FFObject {
+    var primaryID: Int64?
+    
+    static func memoryPropertys() -> [String]? {
+        return nil
+    }
+    
+    static func customColumnsType() -> [String : String]? {
+        return nil
+    }
+    
+    static func customColumns() -> [String : String]? {
+        return nil
+    }
+    
+    var name : String?
+    var creatTime : Date?
+    var address = String()
+    var  test: Float?
+}
+
+struct FFGood :FFObject {
+    var primaryID: Int64?
+    
+    static func memoryPropertys() -> [String]? {
+        return nil
+    }
+    
+    static func customColumnsType() -> [String : String]? {
+        return nil
+    }
+    
+    static func customColumns() -> [String : String]? {
+        return nil
+    }
+    
+    var goodName : String?
+    var creatTime : Date?
+    var price = Float()
+}
